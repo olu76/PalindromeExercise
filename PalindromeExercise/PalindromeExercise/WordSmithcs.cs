f432c4d54832f16d5b0d4971e0e59faf47d4395e
olumide azeez afolabi <midas44000@yahoo.com>
2021-11-28 00:49:30 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmithcs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+   public class WordSmithcs
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >=0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+
+            if(reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("Olumide", false)]
+        [InlineData("rotor", true)]
+        [InlineData("civic", true)]
+
+
+
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var tester = new WordSmithcs();
+
+            //Act
+            var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
